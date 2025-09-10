20849cda9ac1c140a79976e3f37064641361d18e
waste3d <kolya@waste3d.su>
2025-09-10 19:36:02 +0300

    feat(domain): tunnel repository

---
+++ internal/domain/tunnel_repository.go
@@ -0,0 +1,10 @@
+package domain
+
+import "context"
+
+type TunnelRepository interface {
+	Save(ctx context.Context, tunnel *Tunnel) error
+	FindByID(ctx context.Context, id TunnelID) (*Tunnel, error)
+	FindBySubdomain(ctx context.Context, subdomain string) (*Tunnel, error)
+	Delete(ctx context.Context, id TunnelID) error
+}
